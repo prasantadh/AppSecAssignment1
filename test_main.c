fac78cd63a8f0c9b89ab7b4453967c2bddaeead9
prasant adhikari <pa1038@nyu.edu>
2019-09-30 11:45:43 -0400

    Add whitespace is not word check

--- test_main.c
+++ test_main.c
@@ -31,6 +31,7 @@ START_TEST(test_check_word_normal)
     ck_assert(check_word("JuStiCE", hashtable));
     ck_assert(!check_word("", hashtable));
     ck_assert(check_word("K", hashtable));
+    ck_assert(!check_word("    \t\t\t   \n\n\n \r\r\n ", hashtable));
     // ck_assert(!check_word(NULL, hashtable));
     // ck_assert(!check_word(correct_word, NULL));
     // Test here: What if a word begins and ends with "?
